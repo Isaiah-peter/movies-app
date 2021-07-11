ddc544f39878fcf785662de306dc7734a6956fdd
Peter Isaiah <peterisaiah4fun@gmail.com>
2021-07-11 22:10:57 +0100

    adding serchbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import moviedb from '../api/moviesapi'
 import logo from '../img/film.png'
 import user from '../img/user.jpg'
 import menu from '../img/menu.png'
@@ -6,15 +7,45 @@ import close from '../img/close.png'
 import search from '../img/search.png'
 
 
-export class Navbar extends React.Component {
 
-  hide = () => {
+export const Navbar = () => {
+  const  [searchInput, setSearchIput ] = useState('')
+  const [result, setResult] = useState([])
+
+  useEffect(()=>{
+    if(searchInput && !result.length){
+      searchMovies()
+    }else{
+      const timeOutId = setTimeout(()=>{ if(searchInput){
+        searchMovies()
+      }}, 1000)
+
+      return () => {
+        clearTimeout(timeOutId)
+      }
+    }
+  },[searchInput])
+
+  const searchMovies = async() =>{
+    const { data } = await moviedb.get('/search/movie',{
+      params:{
+        query:searchInput
+      }
+    })
+
+    setResult(data.results)
+    console.log(data.results)
+  }
+
+
+
+ const hide = () => {
     const ul = document.getElementById('side-bar')
 
     ul.classList.remove('show')
 
   }
-  show = () => {
+  const show = () => {
     const ul = document.getElementById('side-bar')
     const close = document.getElementById('close')
 
@@ -22,42 +53,60 @@ export class Navbar extends React.Component {
     close.classList.add('cancel')
   }
 
-  con(){
+ const con= () => {
     console.log('rtt')
   }
 
-  render() {
+ const searchBar = (e) =>{
+    setSearchIput(e.target.value)
+  }
+
+  const renderList = result.map((res)=>{
+      return(
+        <li className='navbar__search-output-list'>
+                  <a href={`/moviedetail/${res.id}`} className='navbar__search-output-link'>
+                   {res.original_title}
+                  </a>
+                </li>
+      )
+  })
+
+ 
     return (
       <div className='navbar'>
         <div className='navbar__app-detail'>
           <div className='app-name-icon'>
             <img  src={logo} className='navbar-logo' alt="logo" />
-            <h1 className='app-name'>MoviesApp</h1>
+            <h1 className='app-name'><a style={{textDecoration:'none' ,color:'#fff'}} href='/'>MoviesApp</a></h1>
           </div>
            
           <div id='side-bar' className='navbar__search-list'>
-            <img id='close' className='navbar-logo' onClick={this.hide} src={close} alt='close' />
+            <img id='close' className='navbar-logo' onClick={hide} src={close} alt='close' />
             <div className='navbar-list'>
               <nav>
                 <ul className='navbar-items'>
-                  <li className='navbar-item'><a  onClick={this.hide} href="#">Movies</a></li>
-                  <li className='navbar-item'><a  onClick={this.hide} href="#">TV Shows</a></li>
-                  <li className='navbar-item'><a  onClick={this.hide} href="#">Actors</a></li>
+                  <li className='navbar-item'><a  onClick={hide} href="#">Movies</a></li>
+                  <li className='navbar-item'><a  onClick={hide} href="#">TV Shows</a></li>
+                  <li className='navbar-item'><a  onClick={hide} href="#">Actors</a></li>
                 </ul>
               </nav>
             </div>
             <div className='navbar__search'>
-              <form onSubmit={this.hide,this.con} className='navbar__form'>
+              <form onSubmit={hide,con} className='navbar__form'>
                 <img className='search-icon' alt='search' src={search} />
-                <input className='navbar__input' type="text" placeholder='search...' />
+                <input className='navbar__input' onChange={searchBar} value={searchInput} type="text" placeholder='search...' />
               </form>
               <img className='user' src={user} />
             </div>
+            {searchInput !== "" ? <div className='navbar__search-output'>
+              <ul>
+                 {renderList}
+              </ul>
+            </div>:null}
           </div>
-          <img onClick={this.show} src={menu} alt='menu' className='navbar-logo menu'/>
+          <img onClick={show} src={menu} alt='menu' className='navbar-logo menu'/>
         </div>
       </div>
     )
   }
 
-}
